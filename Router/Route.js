9293b9b3f5657a815bc717ce37912fb761a3f44c
shiva748 <shivagautam2002@gmail.com>
2024-05-27 12:40:17 +0530

    update

--- Router/Route.js
+++ Router/Route.js
@@ -2,6 +2,11 @@ const express = require("express");
 const Router = express.Router();
 const { register, login, authenticate } = require("../Controller/controller");
 const verifyToken = require("../Middleware/auth");
+const path = require("path");
+
+Router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Doc.pdf"));
+});
 
 Router.post("/register", register);
 
